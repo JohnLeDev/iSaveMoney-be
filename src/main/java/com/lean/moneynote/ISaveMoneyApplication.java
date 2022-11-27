2c8e940ce548b33e8465898594781d3d623d316f
JohnLeDev <64694489+JohnLeDev@users.noreply.github.com>
2022-11-27 23:06:52 +0700

    Fist commit

---
+++ src/main/java/com/lean/moneynote/ISaveMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.lean.moneynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISaveMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISaveMoneyApplication.class, args);
+	}
+
+}
