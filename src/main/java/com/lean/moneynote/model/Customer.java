1faf1eb765062d8c5966f938b1bf58d23a915376
Ubuntu <levohoangan2510@gmail.com>
2022-12-04 20:20:15 +0700

    config

---
+++ src/main/java/com/lean/moneynote/model/Customer.java
@@ -0,0 +1,37 @@
+package com.lean.moneynote.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table
+@Entity
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	
+	private String name;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
