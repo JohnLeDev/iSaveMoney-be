420b44036422dfd1a697ee2411a8a0e93fe989a1
Ubuntu <levohoangan2510@gmail.com>
2022-12-23 18:06:22 +0700

    update registration

---
+++ src/main/java/com/lean/moneynote/registration/RegistrationDto.java
@@ -0,0 +1,10 @@
+package com.lean.moneynote.registration;
+
+import java.util.Date;
+
+public class RegistrationDto {
+	private String username; 
+	private String password;
+	private Date dob;
+	private String phonenumber;
+}
